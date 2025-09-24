d1b54948cc743afcea2387af11a708cb5b2167a7
DevServer24 <developmentserver160@gmail.com>
2025-09-24 21:31:52 +0800

    added

--- src/controllers/webhook.ts
+++ src/controllers/webhook.ts
@@ -1,19 +1,33 @@
 import type { Request, Response } from "express";
 import { verifyWebhook } from '@clerk/express/webhooks';
+import db from '../dbconfiguration/db'; // Your database client
+import * as schema from '../drizzle/schema'
 
 export const clerkWebhookHandler = async (req: Request, res: Response) => {
   try {
     const evt = await verifyWebhook(req);
-
-    // Do something with payload
-    // For this guide, log payload to console
     const { id } = evt.data;
     const eventType = evt.type;
     console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
-    console.log('Webhook payload:', evt.data);
+
+    if (eventType === 'user.created') {
+      const userData = evt.data;
+
+      // Insert into your DB
+      await db.insert(schema.users).values({
+        clerkId: userData.id,
+        email: userData.email_addresses?.[0]?.email_address || '',
+        username: userData.first_name || 'Unknown',
+        createdAt: Date.now(), // number
+      });
+      
+      
+
+      console.log(`User ${userData.id} saved to database`);
+    }
 
     return res.send('Webhook received');
-  } catch (err: any) { // Added : any for err
+  } catch (err: any) {
     console.error('Error verifying webhook:', err);
     return res.status(400).send(`Error verifying webhook: ${err.message}`);
   }
